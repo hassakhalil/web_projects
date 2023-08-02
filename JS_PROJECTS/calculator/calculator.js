83c291ba6c970f6c004491c2e0f66627782c8d47
hassan khalil <hassan@hassans-MacBook-Pro-2.local>
2023-08-02 13:40:48 +0100

    initial commit

---
+++ JS_PROJECTS/calculator/calculator.js
@@ -0,0 +1,11 @@
+//listen on for events (button clicks)
+//types of events
+//clear :
+//      clear-entry-> remove the last entry
+//      clear-all-->  reset the number to zero
+//number : add the number associated the the button to the number
+//operator : 
+//           store the first the number somewhere
+//           reset the first variable to 0
+//           wait for the user to enter the second operand
+//           perform the operation  
---
+++ JS_PROJECTS/expirements.js
@@ -0,0 +1,6 @@
+let j=5;
+let answer ='';
+for (let i=0; i<j; i++){
+    answer+='🌎';
+}
+console.log(answer);
